21b0137a493401a3a952d1c45bf685ae1f290c0e
l-PC\l <artemmelnyk@gmail.com>
2017-10-14 18:25:17 +0200

    first version that works

--- test_FSR_class/FSR.cpp
+++ test_FSR_class/FSR.cpp
@@ -23,7 +23,9 @@ FSR::FSR(int pin)
 }
 
 
-void FSR::set_i2c_addr(int address)
+void
+FSR::set_i2c_addr(int address)
+
 { 
   //TO DO check if the adress is correct
     _address = address;
@@ -36,95 +38,91 @@ C'est exactement un programmeur utilisateur du classe connais
 le type de l'equipement et c'est a lui de definir via calibration des coefficients d'approximation
 */
 
-void FSR::set_coef_a(double value)
+void
+FSR::set_coef_a(double value)
+
 { 
     _a = value;
 }
 
-void FSR::set_coef_b(double value)
+void
+FSR::set_coef_b(double value)
+
 { 
     _b = value;
 }
 
-void FSR::set_coef_c(double value)
+void
+FSR::set_coef_c(double value)
+
 { 
     _c = value;
 }
 
-void FSR::set_coef_d(double value)
+void 
+FSR::set_coef_d(double value)
+
 { 
     _d = value;
 }
 
 /* getters */
 
-double FSR::get_coef_a(void)
+double
+FSR::get_coef_a(void)
+
 { 
     return _a;
 }
 
-double FSR::get_coef_b(void)
+double
+FSR::get_coef_b(void)
+
 { 
     return _b;
 }
 
-double FSR::get_coef_c(void)
+double
+FSR::get_coef_c(void)
+
 { 
     return _c;
 }
 
-double FSR::get_coef_d(void)
+double
+FSR::get_coef_d(void)
+
 { 
     return _d;
 }
 
-double FSR::get_force()
-{ 
+double
+FSR::get_force()
+
+{
+  int receivedValue;
+  
   Serial.print ("get_force ");
+  
   Serial.println (_address);
-//  
-//  union asdf
-//  {
-//  char asd[2];
-//  int dsa;
-//  } fdsa;
-//     int  i = 0;
-//  
-//  if (_address == -1){
-//    Serial.println("ERROR, Address does not well defined");
-//    return -1.1;}
-
-
- 
-    
-//  Wire.beginTransmission(_address); // transmit to device #4
-//  Wire.write( _pin);              // sends one byte
-//  Wire.endTransmission();    // stop transmitting
-//  
-//  Serial.println("end transmission");
-   
-  Wire.requestFrom(_address, 2);    // request 10 bytes from slave device #8
-
-
- 
-//  while (Wire.available() && i <= 2) { // slave may send less than requested
-//      Serial.print(i); Serial.print(" ");
-//      fdsa.asd[i] = Wire.read(); // receive a byte as character
-//      i++;
-//    //Serial.print(c);         // print the character
-//  }
-//
-//
-//   //return _a*pow(2.17,_b) + _c*pow(2.17,_d);
-//   return fdsa.dsa;
-int receivedValue;
-     Wire.requestFrom(4, 12);    // request 6 bytes from slave device #2
-  while(Wire.available()){    // slave may send less than requested
+  
+  if (_address == -1)
+  {
+    Serial.println("ERROR, Address does not well defined");
+    return -1.1;
+  }
+  
+  Wire.requestFrom(_address, 12);    // request 12 bytes from slave device #_address
+  
+  while(Wire.available())
+  {    // slave may send less than requested
     receivedValue  = Wire.read() << 8;
     receivedValue |= Wire.read();
     Serial.print(receivedValue);
-Serial.print(" ");  }
-Serial.println();
-
-   return receivedValue;
+    Serial.print(" ");
+  }
+  
+  Serial.println();
+  
+  return receivedValue;
 }
