61d2050e382455fa8889db82ed10047f17a01496
Olga RYBALKO <olyarybalko@gmail.com>
2017-10-21 21:05:52 +0200

    receive implementation in slave controller

--- test_FSR_class/slave_receiver_fsr/slave_receiver_fsr.ino
+++ test_FSR_class/slave_receiver_fsr/slave_receiver_fsr.ino
@@ -1,4 +1,4 @@
-// Wire Slave Sender
+// Wire Slave Sender with Filtering
 // by Olga Melnyk<http://www.>
 
 // Demonstrates use of the Wire library
@@ -8,48 +8,51 @@
 // Created 08 Octobre 2017
 
 // This example code is in the public domain.
-
+//TODO
+//Implement the filter
+//https://www.norwegiancreations.com/2016/03/arduino-tutorial-simple-high-pass-band-pass-and-band-stop-filtering/
 
 #include <Wire.h>
 
-//a range of -32,768 to 32,767
+// for signed int a range of -32,768 to 32,767
+//for unsigned int range of 0 to 65,535 (2^16 - 1)
 unsigned int input = 0;
-  
-void setup()
+int pin = 0;
+
+
+void
+setup()
+
 {
   Wire.begin(4);                // join i2c bus with address #4
-  //Wire.onReceive(receiveEvent); // register event
-   Wire.onRequest(requestEvent); // register event
-  }
-
-void loop()
-{
-delay(100);
+  Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent); // register event
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-//void receiveEvent(int howMany)
-//{
-//
-//  while (1 < Wire.available()) // loop through all but the last
-//  {
-//    int result = 256;
-//   // int x = Wire.read(); // receive byte as a character
-//   // if (x>0 && x<6)
-//   // result = 1;//analogRead(x);
-//  //  else result = -1;
-//    Wire.write(result);
-//   } 
-//}
+void
+loop()
 
-void requestEvent()
+{
+  input = analogRead(pin);
+}
+
+void
+requestEvent()
+
 {
   uint8_t buffer[2];
-  input = 111;//analogRead(0);
   buffer[0] = input >> 8;
   buffer[1] = input & 0xff;
-  
   Wire.write(buffer, 2);
-  
+}
+
+void
+receiveEvent(int howMany)
+{
+  int x;
+  while (1 < Wire.available()) // loop through all but the last
+    x = Wire.read();    // receive byte as an integer
+  x == 1 ? pin = 1 : pin = 0;
+    
+
 }
